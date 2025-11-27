040d644525fc635fbcd92c6b5977eea6759e86d0
双越 <wangfupeng1988@163.com>
2025-11-27 13:51:33 +0800

    chore: local build and start

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -1,17 +1,10 @@
 import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 import { Chat } from "@/components/chat";
 import { DataStreamHandler } from "@/components/data-stream-handler";
 import { DEFAULT_CHAT_MODEL } from "@/lib/ai/models";
 import { generateUUID } from "@/lib/utils";
-import { auth } from "../(auth)/auth";
 
 export default async function Page() {
-  const session = await auth();
-
-  if (!session) {
-    redirect("/api/auth/guest");
-  }
 
   const id = generateUUID();
 
